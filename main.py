fc27763d7507d66f324d97309615555e97c2fea7
Samat <anuarsamatunderworld@gmail.com>
2025-06-16 22:35:43 +0500

    1 pr

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-a = input()
+#1
+"""a = input()
 b = input()
 c = input()
 print(a+b+c)
+Answer : Программирование после школы!
+"""
+
+#2
+
+"""
+a = int(input())
+b = int(input())
+print(2**8 * a**8 - 2**4 * a**4 + 2**2 * a ** 2 - 2*b +1/2 * b**(1/2) + (a*b)**(b+a)/2)
+
+Answer :32134205104895.5
+"""
+
+
+#3 Ответ: 23!
+
+#4 ответ:
+"""
+(123 + 456 - 789) % 10 = 0
+"""
+# 5
+"""
+n = int(input())
+print(n+1, n-1, sep = '')
+Answer: 2523
+"""
+"""
+
+Sm = int(input())
+Km = Sm // 100000
+print(Km)
+Answer : 1234
+"""
